f7b2dd9858476e7dc897b4b6e872d9b87c7d61b9
mohd <mohammad2java@gmail.com>
2026-05-16 21:07:24 +0530

    first commit

---
+++ src/main/java/com/example/argocd_yml_demo/ArgocdYmlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.argocd_yml_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdYmlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgocdYmlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/argocd_yml_demo/ArgocdYmlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.argocd_yml_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgocdYmlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
